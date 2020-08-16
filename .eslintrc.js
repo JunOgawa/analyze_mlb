f01dcb2619c7bd70843ed4fa6db038fec9c72eb5
JunOgawa <jogawa@gmail.com>
2020-08-16 10:13:41 +0900

    add .eslitrc.js and change the name of CSS classes
    - Creating .eslintrc.json which nicely works with prettier.
    - Change the class names of CSS to camelCase to kebab-case.
    - Consolidate the  name of JS to camelCase

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  extends: ['prettier', 'plugin:prettier/recommended'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+    ecmaVersion: 2015
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        trailingComma: 'none',
+        tabWidth: 2,
+        semi: false,
+        singleQuote: true
+      }
+    ]
+  }
+}
+/* 
+1) This is the basic to let eslink work with prettier.
+$ npm i -D eslint-plugin-prettier eslint-config-prettier prettier
+
+module.exports = {
+  extends: ["plugin:prettier/recommended"],
+}
+
+See: https://blog.ojisan.io/eslint-prettier
+
+2) If you face "Parsing error: The keyword 'import' is reserved"
+when do "npm run serve", put the following:
+
+module.exports = {
+   // ・・・
+   parserOptions: {
+     sourceType: "module",
+     ecmaVersion: 2015,
+   },
+};
+
+See: https://qiita.com/kozzzz/items/be3a5f326311b6784860
+See: https://numb86-tech.hatenablog.com/entry/2018/09/17/185100
+
+3) Add rules as same as .prettierrc. You need both .prettierrc and .eslintrc.js
+
+4) add babel.config.js as follows:
+module.exports = {
+  presets: [['@vue/app', { useBuiltIns: 'entry' }]]
+}
+
+*/
