0a8072d879f107d31ae48ddf23597000f28da749
JunOgawa <jogawa@gmail.com>
2020-08-24 19:11:21 +0900

    Power BI view from the beginning.
    Display Power BI view when opening this webpage.
    Added HOU players.

--- src/components/PAnalysis.vue
+++ src/components/PAnalysis.vue
@@ -127,29 +127,18 @@ export default {
     // set initial value of selectedTeam.
     // If there's no AL team in pichtersData, that will be in trouble.
     this.selectedTeam = this.ALTeams[0].teams[0]
+
+    this.changePlayer(
+      this.choosePlayers(this.pitchersData, this.selectedYear, this.selectedTeam)[0]
+    )
   },
 
   computed: {
-    // I'm not sure why only the following can move to computed
-    // but others are not.
-    // Take care you cannot directly use any arguments in function
-    // in computed, but use like the followings.
+    // If you use a method only in between <template> and </template>,
+    // you can put the method to computed: area.
+    // But methods also used in created, you should put them to metods:.
 
     //  Picking out players in the selected year and teams.
-    choosePlayers: () => {
-      return (val, target1, target2) => {
-        let retVal = []
-        retVal = val
-          .filter((item) => {
-            return item.year === target1 && item.team === target2
-          })
-          .map((item) => item.name)
-          .sort()
-        // retVal2 = retVal.map(item => item.name)
-        return retVal
-      }
-    },
-
     //  Picking out teams in the selected year.
     chooseTeams: () => {
       return (val, target) => {
@@ -197,6 +186,17 @@ export default {
       if (this.resultURL.length > 1) {
         console.log('result URL Error')
       }
+    },
+    choosePlayers(val, target1, target2) {
+      let retVal = []
+      retVal = val
+        .filter((item) => {
+          return item.year === target1 && item.team === target2
+        })
+        .map((item) => item.name)
+        .sort()
+      // retVal2 = retVal.map(item => item.name)
+      return retVal
     },
 
     // uniqueTeam creates the array like [year:2019, teams: {NYY, BOS}]
